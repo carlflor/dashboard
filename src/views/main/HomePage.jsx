ae94161d79f3b362105e0b3259d41399e5eb0400
Carl Flor <carlmyerflor@gmail.com>
2019-01-14 10:06:37 +0800

    add logout

--- src/views/main/HomePage.jsx
+++ src/views/main/HomePage.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import { withAppContext } from '../../context.js';
 import './HomePage.scss';
 
 class HomePage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.logout = this.logout.bind(this);
+  }
+
+  logout() {
+    this.props.logout();
+  }
+
   render() {
     return (
       <div className="App">
         Hello Home
+        <button onClick={this.logout}>Log out</button>
       </div>
     );
   }
 }
 
-export default HomePage;
+export default withAppContext(HomePage);
